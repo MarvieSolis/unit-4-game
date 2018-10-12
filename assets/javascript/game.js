c94fee644e0d77dbcc8095f4d189594cdb3b0e17
Marvie Solis <marvie_solis@ymail.com>
2018-10-11 17:12:33 -0700

    Converted code to ES6

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,40 @@
 
 
 // Initialize Variables
-var wins = 0;
-var losses = 0;
-var totalScore = 0;
-var randomNumber = 0;
-var stone1 = 0;
-var stone2 = 0;
-var stone3 = 0;
-var stone4 = 0;
-var stone5 = 0;
-var stone6 = 0;
+let wins = 0;
+let losses = 0;
+let totalScore = 0;
+let randomNumber = 0;
+let stone = [];
 
 
-// Functions
-function gameStart() {
-    gameStatus = 0;
+
+// Starts the game and randommizes the numbers
+const gameStart = () => {
+    stone = [];
     totalScore = 0;
     randomNumber = Math.floor(Math.random()*120)+12;
     document.querySelector("#randomnumber").innerHTML = randomNumber;
 
-
-    do{
-    stone1 = Math.floor(Math.random()*12)+1;
+    // Randomly generates a number for a stone and pushes it if it doesnt exist in the array
+    const generate = () => {
+        let x = Math.floor(Math.random()*12)+1;
+        stone.indexOf(x) >= 1 ? generate(0) : stone.push(x);
     }
-    while (stone1 === stone2 || stone1 === stone3 || stone1 === stone4 || stone1 === stone5 || stone1 === stone6);
 
-    do{
-        stone2 = Math.floor(Math.random()*12)+1;
+    // Goes through each stone and runs the generate number
+    for(let i = 0; i < 6; i++) {
+        generate();
     }
-    while (stone2 === stone1 || stone2 === stone3 || stone2 === stone4 || stone2 === stone5 || stone2 === stone6);
-
-    do{
-        stone3 = Math.floor(Math.random()*12)+1;
-    }
-    while (stone3 === stone1 || stone3 === stone2 || stone3 === stone4 || stone3 === stone5 || stone3 === stone6);
-
-    do{
-        stone4 = Math.floor(Math.random()*12)+1;
-    }
-    while (stone4 === stone1 || stone4 === stone2 || stone4 === stone3 || stone4 === stone5 || stone4 === stone6);
-
-    do{
-        stone5 = Math.floor(Math.random()*12)+1;
-    }
-    while (stone5 === stone1 || stone5 === stone2 || stone5 === stone3 || stone5 === stone4 || stone5 === stone6);
-
-    do{
-        stone6 = Math.floor(Math.random()*12)+1;
-    }
-    while (stone6 === stone1 || stone6 === stone2 || stone6 === stone3 || stone6 === stone4 || stone6 === stone5);
 }
 
-
-function test() {
+// Check if the user score is above or equal to the target score
+const test = () => {
     if (randomNumber < totalScore) {
         losses++;
         document.querySelector("#losses").innerHTML = "Losses: " + losses;
         alert("You are not worthy...");
+        document.querySelector("#totalscore").innerHTML = 0;
         gameStart();
     }
 
@@ -65,6 +42,7 @@ function test() {
         wins++;
         document.querySelector("#wins").innerHTML = "Wins: " + wins;
         alert("You are a god amongst us all");
+        document.querySelector("#totalscore").innerHTML = 0;
         gameStart();
     }
 }
@@ -85,37 +63,37 @@ $(document).ready(function() {
 
 
     $("#stone1").on("click", function() {
-        totalScore += stone1;
+        totalScore += stone[0];
         document.querySelector("#totalscore").innerHTML = totalScore;
         test();
     });
 
     $("#stone2").on("click", function() {
-        totalScore += stone2;
+        totalScore += stone[1];
         document.querySelector("#totalscore").innerHTML = totalScore;
         test();
     });
 
     $("#stone3").on("click", function() {
-        totalScore += stone3;
+        totalScore += stone[2];
         document.querySelector("#totalscore").innerHTML = totalScore;
         test();
     });
 
     $("#stone4").on("click", function() {
-        totalScore += stone4;
+        totalScore += stone[3];
         document.querySelector("#totalscore").innerHTML = totalScore;
         test();
     });
 
     $("#stone5").on("click", function() {
-        totalScore += stone5;
+        totalScore += stone[4];
         document.querySelector("#totalscore").innerHTML = totalScore;
         test();
     });
 
     $("#stone6").on("click", function() {
-        totalScore += stone6;
+        totalScore += stone[5];
         document.querySelector("#totalscore").innerHTML = totalScore;
         test();
     });
